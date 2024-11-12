a09cbd54dd5164884bfac6b3f2beb6c4c1b867e0
Fahim Ahmed Emon <ahmed.fahim37@gmail.com>
2024-11-12 20:27:36 +0600

    adding suspense

--- app/dashboard/upgrade/upgrade-success/page.js
+++ app/dashboard/upgrade/upgrade-success/page.js
@@ -1,29 +1,31 @@
 "use client";
-import { useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { toast } from 'sonner';
-import { useMutation } from 'convex/react';
-import { api } from '@/convex/_generated/api';
-import { useUser } from '@clerk/nextjs';
+import { Suspense, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { toast } from "sonner";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useUser } from "@clerk/nextjs";
 
-const UpgradeSuccess = () => {
+const UpgradeSuccessContent = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
     const { user } = useUser();
     const upgradeUserPlan = useMutation(api.user.userUpgrade);
 
     useEffect(() => {
-        const sessionId = searchParams.get('session_id');
+        const sessionId = searchParams.get("session_id");
+
         const verifyAndUpgrade = async () => {
             if (sessionId) {
                 try {
                     const response = await fetch(`/api/verify-payment?session_id=${sessionId}`);
                     const data = await response.json();
+
                     if (data.success) {
-                        const userEmail = data.userEmail
+                        const userEmail = data.userEmail;
                         await upgradeUserPlan({ userEmail });
                         toast("Plan Upgradation Successful");
-                        setTimeout(() => router.push('/dashboard'), 1000);
+                        setTimeout(() => router.push("/dashboard"), 1000);
                     } else {
                         toast("Payment verification failed.");
                     }
@@ -37,10 +39,14 @@ const UpgradeSuccess = () => {
         verifyAndUpgrade();
     }, []);
 
+    return <h2>Processing your upgrade, You will be redirected soon...</h2>;
+};
+
+const UpgradeSuccess = () => {
     return (
-        <div>
-            <h2>Processing your upgrade, You will be redirected soon...</h2>
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <UpgradeSuccessContent />
+        </Suspense>
     );
 };
 
